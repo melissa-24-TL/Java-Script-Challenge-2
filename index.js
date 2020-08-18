1dabee45d9bf47ecb8e1e579a4f8624532220555
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-18 14:57:49 -0400

    another challenge

---
+++ index.js
@@ -0,0 +1,51 @@
+// VAR
+
+// Declare a var and then updated it
+
+console.log();
+
+//LET
+
+// Declare a let and then update it
+
+
+console.log();
+
+
+
+//Function Declaration
+
+// Declare a function that adds 2 numbers
+function addition() {
+    
+  }
+  
+  console.log();
+  
+  //Function Expression
+
+  // Use a function expression that multiplies 2 numbers
+ 
+  
+  console.log();
+  
+  
+  //Arrow Function
+
+  // Use arrow function to divide 2 numbers
+  
+  
+  console.log();
+  
+  
+  //IIFE Function
+/*
+IIFE (Imediatly Invoked Function Expression)
+- Does not require an invocation when referanced
+*/
+
+// Create a function that will return a string of 'I was invoked!'
+
+
+  
+  console.log();
